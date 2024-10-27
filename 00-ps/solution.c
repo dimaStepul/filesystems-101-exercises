00e58d1765d904411291b4585cb4596b29d24f87
dimaStepul <trushaka2001@gmail.com>
2024-10-27 16:16:45 +0200

    oo-ps debil 5

--- 00-ps/solution.c
+++ 00-ps/solution.c
@@ -9,7 +9,7 @@
 #include <unistd.h>
 #include <limits.h>
 
-#define BUFFER_SIZE 65536
+#define BUFFER_SIZE 16384
 #define PATH_MAX 4096
 #define PROC_DIRECTORY "/proc"
 
@@ -32,16 +32,16 @@ DIR *open_proc_dir() {
 	return proc_dir;
 }
 
-// void *allocate_buffer(size_t size) {
-// 	void *buffer = malloc(size + 1);
-// 	if (!buffer) {
-// 		free(buffer);
-// 		report_error(PROC_DIRECTORY, ENOMEM);
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	memset(buffer, 0, size);
-// 	return buffer;
-// }
+void *allocate_buffer(size_t size) {
+	void *buffer = malloc(size + 1);
+	if (!buffer) {
+		free(buffer);
+		report_error(PROC_DIRECTORY, ENOMEM);
+		exit(EXIT_FAILURE);
+	}
+	memset(buffer, 0, size);
+	return buffer;
+}
 
 
 ssize_t get_executable_path(pid_t pid, char *exe_buf) {
@@ -49,9 +49,8 @@ ssize_t get_executable_path(pid_t pid, char *exe_buf) {
 	snprintf(path_buf, sizeof(path_buf), "/proc/%d/exe", pid);
 	ssize_t exe_len = readlink(path_buf, exe_buf, PATH_MAX);
 	if (exe_len == -1) {
-		if (errno == EACCES)
-			return -1;
 		report_error(path_buf, errno);
+		return -1;
 	}
 	exe_buf[exe_len] = '\0';
 	return exe_len;
@@ -91,11 +90,8 @@ int parse_strings(char *input, char **output, size_t max_output_size){
 
 
 
-void get_process_info(pid_t pid, char *exe_buf, char **argv_buf, char **envp_buf){
-	if (get_executable_path(pid, exe_buf) == -1)
-		return;
-
-	char path_buf[256];
+void get_process_info(pid_t pid, char **argv_buf, char **envp_buf){
+	char path_buf[PATH_MAX];
 	char argv_read[BUFFER_SIZE];
 	char envp_read[BUFFER_SIZE];
 
@@ -118,8 +114,8 @@ void ps(void)
 	if (!proc_dir)
 		return;
 	char exe_buf[PATH_MAX];
-	char **argv_buf = malloc((BUFFER_SIZE / sizeof(char *)) * sizeof(char *));
-	char **envp_buf = malloc((BUFFER_SIZE / sizeof(char *)) * sizeof(char *));
+	char **argv_buf = allocate_buffer(BUFFER_SIZE / sizeof(char *));
+	char **envp_buf = allocate_buffer(BUFFER_SIZE / sizeof(char *));
 	if (!argv_buf || !envp_buf)
 	{
 		free(argv_buf);
@@ -134,7 +130,10 @@ void ps(void)
 			continue;
 
 		pid_t pid = atol(cur_dir->d_name);
-		get_process_info(pid, exe_buf, argv_buf, envp_buf);
+		if (get_executable_path(pid, exe_buf) == -1) {
+			continue;
+		}
+		get_process_info(pid, argv_buf, envp_buf);
 		report_process(pid, exe_buf, argv_buf, envp_buf);
 		errno = 0;
 	}
